05aa98739a32f6f4e926c6ab69d3ce91ac57dd32
Rinat <fanar13@gmail.com>
2026-02-27 00:45:34 +0500

    add parsing command

--- internal/service/parsingCommand_test.go
+++ internal/service/parsingCommand_test.go
@@ -116,7 +116,17 @@ func Test_parseLinesMap(t *testing.T) {
 		},
 		{
 			name: "add 10 lines",
-			args: args{"1:\"1 text\", 2:\"2 text\", 3:\"3 text\", 4:\"4 text\", 5:\"5 text\", 6:\"6 text\", 7:\"7 text\", 8:\"8 text\", 9:\"9 text\", 10:\"10 text\""},
+			args: args{
+				"1:\"1 text\"," +
+					" 2:\"2 text\"," +
+					" 3:\"3 text\"," +
+					" 4:\"4 text\"," +
+					" 5:\"5 text\"," +
+					" 6:\"6 text\"," +
+					" 7:\"7 text\"," +
+					" 8:\"8 text\"," +
+					" 9:\"9 text\"," +
+					" 10:\"10 text\""},
 			want: map[int]string{
 				1:  "1 text",
 				2:  "2 text",
